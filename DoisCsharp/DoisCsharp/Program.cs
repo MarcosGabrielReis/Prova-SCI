3903c4d11021183dcd0c1c6ecec08c5622a04b84
marcosreis2019 <marcos.reis2019@hotmail.com>
2023-03-17 20:34:28 -0300

    Resposta da Questão 02

---
+++ DoisCsharp/DoisCsharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoisCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5];
+            int menor = int.MaxValue;
+            int maior = int.MinValue;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Digite o " + (i + 1) + "º número: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+
+                if (numeros[i] < menor)
+                {
+                    menor = numeros[i];
+                }
+
+                if (numeros[i] > maior)
+                {
+                    maior = numeros[i];
+                }
+            }
+
+            Console.WriteLine("\nO menor número é: " + menor);
+            Console.WriteLine("O maior número é: " + maior);
+
+            Console.ReadKey();
+
+        }
+    }
+}
