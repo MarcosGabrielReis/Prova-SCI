079b30b0073d36af1ae605f14b052e9881fae4bf
marcosreis2019 <marcos.reis2019@hotmail.com>
2023-03-17 20:35:51 -0300

    Resposta da Questão 03

---
+++ TresCsharp/TresCsharp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TresCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Digite o nome do aluno (ou 'sair' para encerrar o programa): ");
+                string nome = Console.ReadLine();
+
+                if (nome.ToLower() == "sair")
+                {
+                    break;
+                }
+
+                double soma = 0;
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.Write("Digite a " + (i + 1) + "ª nota: ");
+                    double nota = double.Parse(Console.ReadLine());
+                    soma += nota;
+                }
+
+                double media = soma / 4;
+                Console.WriteLine("A média do aluno " + nome + " é " + media);
+
+                if (media >= 6)
+                {
+                    Console.WriteLine("APROVADO");
+                }
+                else
+                {
+                    Console.WriteLine("REPROVADO");
+                }
+                Console.WriteLine();
+
+
+            }
+        }
+    }
+}
