c2277e9aec3cab318add306c32c5ab7c9dc8b60b
marcosreis2019 <marcos.reis2019@hotmail.com>
2023-03-17 20:36:56 -0300

    Resposta da Questão 04

---
+++ QuatroCsharp/QuatroCsharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuatroCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Digite o " + (i + 1) + "º número: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nO valor apresentado na posição três é: " + numeros[2]);
+
+            Console.ReadKey();
+
+        }
+    }
+}
